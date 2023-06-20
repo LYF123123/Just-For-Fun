564d437e9e7d401624946a727a822f0fe460fe99
lyf <luyifeng3@163.com>
2023-06-20 18:45:52 +0800

    new cpp

---
+++ Я Ру́сский/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#define ярусский setlocale(LC_CTYPE,"Russian")
+#define блять ;
+#define нихуя void
+#define факт bool
+#define базарь cout<<
+#define спроси cin>>
+#define пиздато 1
+#define если if(
+#define то )
+#define сука ==
+#define иначе else
+#define типа {
+#define ёба }
+#define ану_иди_сюда using namespace
+#define сказал std;
+#define стопе main()
+#define СЕМКИ SEMKI
+
+ану_иди_сюда сказал блять
+
+нихуя стопе
+типа
+    ярусский блять
+    типа
+    факт СЕМКИ блять
+        базарь "Семки есть, ёба?\n" блять
+        спроси СЕМКИ блять
+        если СЕМКИ сука пиздато то
+            типа
+                базарь "Красава\n" блять
+            ёба
+            иначе
+            типа
+                базарь "Нарывашься, сука?!\n" блять
+            ёба
+        ёба
+    ёба
+ёба
+
