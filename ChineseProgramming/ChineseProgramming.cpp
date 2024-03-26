b3fea1fc79f04bba287a0c57247fdb113ee2230a
lyf <luyifeng3@163.com>
2024-03-26 10:56:36 +0800

    add ChineseProgramming

---
+++ ChineseProgramming/ChineseProgramming.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define 整数 int
+#define 主 main
+#define 标准 std::
+#define 字符输出 cout
+#define 位移位 <<
+#define 分号 ;
+#define 添加 additions
+#define 加 +
+#define 返回 return 
+#define 一 1
+#define 二 2
+#define 三 3
+#define 四 4
+#define 五 5
+#define 六 6
+#define 七 7
+#define 八 8
+#define 九 9
+#define 十 10
+
+#define 左圆括号 (
+#define 右圆括号 )
+#define 左中括号 [
+#define 右中括号 ]
+#define 左大括号 {
+#define 右大括号 }
+#define 逗号 ,
+
+
+
+整数 添加 左圆括号 整数 s 逗号 整数 b 右圆括号 左大括号
+    返回 s 加 b 分号
+右大括号
+
+整数 主 左圆括号 右圆括号 左大括号
+    标准 字符输出 位移位 添加 左圆括号 5 逗号 7 右圆括号 分号
+右大括号
